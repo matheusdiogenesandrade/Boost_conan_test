64b46777c6c03ad0cef901a0bf48870dfb1881b8
Matheus Diógenes Andrade <matheusdiogenesandrade@pop-os.localdomain>
2023-08-28 20:03:51 +0200

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
 
+namespace logging = boost::log;
+namespace keywords = boost::log::keywords;
+
+void init_logging() {
+//    logging::register_simple_formatter_factory<logging::trivial::severity_level, char>("Severity");
+
+    logging::add_file_log(
+            keywords::file_name = "sample.log",
+            keywords::auto_flush = true
+//            , keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] [%ProcessID%] [%LineID%] %Message%"
+            );
+
+   logging::add_common_attributes();
+}
 
 int main (int argc, const char *argv[])
 {
+    init_logging();
+
+    BOOST_LOG_TRIVIAL(debug) << "Test";
     return 0;
 }
 
